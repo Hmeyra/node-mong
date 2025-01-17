c327c43d9981d2f22463334ee1f6f0c56717bf57
hmeyra ceyhan <hmeyra@hmeyra-MacBook-Pro.local>
2025-01-17 16:20:02 +0300

    user auth yapısı kuruldu #10

--- api/config/index.js
+++ api/config/index.js
@@ -1,5 +1,9 @@
 module.exports = {
     "PORT": process.env.PORT || 3000,
     "LOG_LEVEL" : process.env.LOG_LEVEL || "debug",
-    "CONNECTION_STRING" : process.env.CONNECTION_STRING || "mongodb://localhost:27017/dbname"
+    "CONNECTION_STRING" : process.env.CONNECTION_STRING || "mongodb://localhost:27017/dbname",
+    "JWT": {
+        "SECRET": "12345",
+        "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60, //Bir gün
+    }
 }
